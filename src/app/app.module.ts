8af5be952901dd97296722cb532240fe6027d194
Taki Uddin <takiuddin93@gmail.com>
2022-01-26 23:34:33 +0600

    new Angular CLI updated to version 13.1.4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NavigationComponent } from './navigation/navigation.component';
-import { IntroductionComponent } from './introduction/introduction.component';
-import { AboutComponent } from './about/about.component';
-import { SideButtonsComponent } from './side-buttons/side-buttons.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from '../navigation/navigation.component';
+import { SideButtonsComponent } from '../side-buttons/side-buttons.component';
+import { IntroductionComponent } from '../introduction/introduction.component';
+import { FooterComponent } from '../footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    IntroductionComponent,
-    AboutComponent,
     SideButtonsComponent,
-    ExperienceComponent,
+    IntroductionComponent,
     FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
